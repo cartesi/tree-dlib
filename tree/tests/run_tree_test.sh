69c76ce56d166472e0a4d681c8eb21f5f87d3b03
Stephen Chen <stephen.chen@cartesi.io>
2021-04-29 00:17:12 +0800

    Clean up test and deployment script

--- tree/tests/run_tree_test.sh
+++ tree/tests/run_tree_test.sh
@@ -7,7 +7,10 @@ sleep 3
 echo '{"jsonrpc":"2.0","method":"personal_newAccount","params":[""],"id":67}' | socat /tmp/geth.ipc -
 
 # Deploy contracts to localhost
-npx hardhat deploy --export deployment.json --network localhost --reset --tags TestTree
+rm TestTree.address
+npx hardhat deploy --network localhost --reset
+npx hardhat run --network localhost ../../scripts/deploy_test_tree.ts
+sleep 3
 
 # Run tests flagged with ingored
 cargo test -p tree --test tree_test -- --nocapture --ignored
--- tree/tests/tree_test.rs
+++ tree/tests/tree_test.rs
@@ -1,16 +1,11 @@
 use dispatcher::state_fold::{Access, StateFold};
-// use dispatcher::types::*;
 use tree::fold::tree_delegate::TreeFoldDelegate;
 
-// use ethabi::Token;
 use ethers::providers::{Http, Middleware, Provider};
 use ethers::types::{U256, U64};
 use std::convert::TryFrom;
 use std::fs;
 use std::sync::Arc;
-// use web3::types::{BlockId, BlockNumber, Bytes, TransactionRequest, U256};
-
-static CONTRACT: &'static str = "TestTree";
 
 // $ geth --dev --http --http.api eth,net,web3
 static HTTP_URL: &'static str = "http://localhost:8545";
@@ -19,25 +14,10 @@ static HTTP_URL: &'static str = "http://localhost:8545";
 #[tokio::test]
 #[ignore]
 async fn tree_test() {
-    // @dev can we add this function to dispatcher-util?
     // getting address from hardhat deployment file
     let test_tree_address: ethers::types::Address = {
-        let s = fs::read_to_string("tests/deployment.json").unwrap();
-
-        let json: serde_json::Value = serde_json::from_str(&s).unwrap();
-        let address_str = &json["contracts"][CONTRACT]["address"];
-
-        let address_str = serde_json::to_string(address_str).unwrap();
-
-        let address_str = {
-            // Remove leading quotes and '0x', and remove trailing quotes
-            let leading_offset = 3;
-            let len = address_str.len() - 1;
-
-            &address_str[leading_offset..len]
-        };
-
-        address_str.parse().unwrap()
+        let s = fs::read_to_string("tests/TestTree.address").unwrap();
+        s.parse().unwrap()
     };
 
     let provider = Arc::new(Provider::<Http>::try_from(HTTP_URL).unwrap());
