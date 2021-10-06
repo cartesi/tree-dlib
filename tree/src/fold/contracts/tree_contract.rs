b5d8c5f300528ec365e28fc8a278e467259a2cb3
Stephen Chen <stephen.chen@cartesi.io>
2021-10-06 14:59:31 +0800

    Bump ethers 0.5

--- tree/src/fold/contracts/tree_contract.rs
+++ tree/src/fold/contracts/tree_contract.rs
@@ -1,51 +1,48 @@
 pub use tree_mod::*;
 #[allow(clippy::too_many_arguments)]
 mod tree_mod {
+    #![allow(clippy::enum_variant_names)]
     #![allow(dead_code)]
     #![allow(unused_imports)]
-    use ethers::{
-        contract::{
-            self as ethers_contract,
-            builders::{ContractCall, Event},
-            Contract, Lazy,
-        },
-        core::{
-            self as ethers_core,
-            abi::{Abi, Detokenize, InvalidOutputType, Token, Tokenizable},
-            types::*,
-        },
-        providers::{self as ethers_providers, Middleware},
+    use ethers::contract::{
+        builders::{ContractCall, Event},
+        Contract, Lazy,
     };
+    use ethers::core::{
+        abi::{Abi, Detokenize, InvalidOutputType, Token, Tokenizable},
+        types::*,
+    };
+    use ethers::providers::Middleware;
     #[doc = "Tree was auto-generated with ethers-rs Abigen. More information at: https://github.com/gakonst/ethers-rs"]
     use std::sync::Arc;
-    pub static TREE_ABI: ethers_contract::Lazy<ethers_core::abi::Abi> =
-        ethers_contract::Lazy::new(|| {
+    pub static TREE_ABI: ethers::contract::Lazy<ethers::core::abi::Abi> =
+        ethers::contract::Lazy::new(|| {
             serde_json :: from_str ("[{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"internalType\":\"uint256\",\"name\":\"_id\",\"type\":\"uint256\"},{\"indexed\":false,\"internalType\":\"uint32\",\"name\":\"_parent\",\"type\":\"uint32\"}],\"name\":\"VertexInserted\",\"type\":\"event\"}]") . expect ("invalid abi")
         });
     #[derive(Clone)]
-    pub struct Tree<M>(ethers_contract::Contract<M>);
+    pub struct Tree<M>(ethers::contract::Contract<M>);
     impl<M> std::ops::Deref for Tree<M> {
-        type Target = ethers_contract::Contract<M>;
+        type Target = ethers::contract::Contract<M>;
         fn deref(&self) -> &Self::Target {
             &self.0
         }
     }
-    impl<M: ethers_providers::Middleware> std::fmt::Debug for Tree<M> {
+    impl<M: ethers::providers::Middleware> std::fmt::Debug for Tree<M> {
         fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
             f.debug_tuple(stringify!(Tree))
                 .field(&self.address())
                 .finish()
         }
     }
-    impl<'a, M: ethers_providers::Middleware> Tree<M> {
+    impl<'a, M: ethers::providers::Middleware> Tree<M> {
         #[doc = r" Creates a new contract instance with the specified `ethers`"]
         #[doc = r" client at the given `Address`. The contract derefs to a `ethers::Contract`"]
         #[doc = r" object"]
-        pub fn new<T: Into<ethers_core::types::Address>>(
+        pub fn new<T: Into<ethers::core::types::Address>>(
             address: T,
             client: ::std::sync::Arc<M>,
         ) -> Self {
-            let contract = ethers_contract::Contract::new(
+            let contract = ethers::contract::Contract::new(
                 address.into(),
                 TREE_ABI.clone(),
                 client,
@@ -55,23 +52,25 @@ mod tree_mod {
         #[doc = "Gets the contract's `VertexInserted` event"]
         pub fn vertex_inserted_filter(
             &self,
-        ) -> ethers_contract::builders::Event<M, VertexInsertedFilter> {
+        ) -> ethers::contract::builders::Event<M, VertexInsertedFilter>
+        {
             self.0.event()
         }
-        #[doc = r" Returns an [`Event`](ethers_contract::builders::Event) builder for all events of this contract"]
+        #[doc = r" Returns an [`Event`](#ethers_contract::builders::Event) builder for all events of this contract"]
         pub fn events(
             &self,
-        ) -> ethers_contract::builders::Event<M, VertexInsertedFilter> {
+        ) -> ethers::contract::builders::Event<M, VertexInsertedFilter>
+        {
             self.0.event_with_filter(Default::default())
         }
     }
     #[derive(
-        Clone, Debug, Default, Eq, PartialEq, ethers_contract :: EthEvent,
+        Clone, Debug, Default, Eq, PartialEq, ethers :: contract :: EthEvent,
     )]
     #[ethevent(name = "VertexInserted", abi = "VertexInserted(uint256,uint32)")]
     pub struct VertexInsertedFilter {
         #[ethevent(indexed)]
-        pub id: ethers_core::types::U256,
+        pub id: ethers::core::types::U256,
         pub parent: u32,
     }
 }
